95e5b6f0ec0b53359c833a9c65a54c6580a72508
yotamhermes <yotamhermes@gmail.com>
2023-09-04 13:24:38 +0300

    add sql

---
+++ postgresql/ddl.sql
@@ -0,0 +1,21 @@
+-- Create the 'ideas' table
+CREATE TABLE ideas (
+    idea_id SERIAL PRIMARY KEY,
+    idea TEXT
+);
+
+-- Create the 'prompts' table
+CREATE TABLE prompts (
+    prompt_id SERIAL PRIMARY KEY,
+    idea_id INT REFERENCES ideas(idea_id),
+    prompt_number INT,
+    prompt_text TEXT
+);
+
+-- Create the 'suggestions' table with foreign key references
+CREATE TABLE suggestions (
+    suggestion_id SERIAL PRIMARY KEY,
+    prompt_id INT REFERENCES prompts(prompt_id),
+    caption TEXT,
+    image_link TEXT
+);
