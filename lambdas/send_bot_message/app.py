c73556a7d8d41583d867748aea376ccbb8ff29ee
yotamhermes <yotamhermes@gmail.com>
2023-09-08 19:04:30 +0300

    added lambda function that invokes a message

---
+++ lambdas/send_bot_message/app.py
@@ -0,0 +1,29 @@
+import json
+import requests
+import os
+
+
+def lambda_handler(event, context):
+
+    CHAT_ID = os.getenv('CHAT_ID')
+    BOT_TOKEN = os.environ.get('BOT_TOKEN')
+
+    message = event.get('message')
+
+    url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
+    params = {
+        'chat_id': CHAT_ID,
+        'text': message
+    }
+
+    response = requests.post(url, data=params)
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(
+            {
+                "response_text": response.text,
+                "response_status_code": response.status_code
+            }
+        ),
+    }
