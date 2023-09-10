9ee5dbcfb29594fb4d3c99e5a3361b3fdd0db1a3
yotamhermes <yotamhermes@gmail.com>
2023-09-10 15:16:47 +0300

    added lambda function

--- lambdas/process_post_ideas/app.py
+++ lambdas/process_post_ideas/app.py
@@ -1,8 +1,7 @@
 import os
 import openai
 import json
-
-openai.api_key = os.getenv("OPENAI_API_KEY")
+import psycopg2
 
 
 def lambda_handler(event, context):
@@ -23,10 +22,27 @@ def lambda_handler(event, context):
 
 
 def get_new_ideas_from_db():
-    return ['Dog dressed as barby doll', 'Dog in paris']
+    DB_CONNECTION_STRING = os.getenv('DB_CONNECTION_STRING')
+
+    try:
+        conn = psycopg2.connect(DB_CONNECTION_STRING)
+
+        cursor = conn.cursor()
+        query = "select idea from post_ideas where status='NEW' and idea is not null"
+
+        cursor.execute(query)
+
+        post_ideas = cursor.fetchall()
+        post_ideas = [i[0] for i in post_ideas]
+
+        return post_ideas
+    except (Exception, psycopg2.Error) as error:
+        print(f"Error fetching ideas: {error}")
 
 
 def generate_prompts_from_ideas(ideas):
+    # openai.api_key = os.getenv("OPENAI_API_KEY")
+
     message = f"""       
         Generate a 5 item's list of Image generation prompts for each of the following general ideas: 
         {ideas}
@@ -34,10 +50,11 @@ def generate_prompts_from_ideas(ideas):
         Seperate characteristics with comas.
     """
 
-    chat_completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": message}]
-    )
+    # chat_completion = openai.ChatCompletion.create(
+    #     model="gpt-3.5-turbo",
+    #     messages=[{"role": "user", "content": message}]
+    # )
+
 
 def save_prompts_to_db(prompts):
     return "saved"
