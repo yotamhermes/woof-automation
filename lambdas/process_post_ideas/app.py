d85e7f7d3167c80fba38d5a7004416c56edc0ab0
yotamhermes <yotamhermes@gmail.com>
2023-09-12 10:41:50 +0300

    added error handling and fixing not changing to DONE bug

--- lambdas/process_post_ideas/app.py
+++ lambdas/process_post_ideas/app.py
@@ -2,6 +2,8 @@ import os
 import ai21
 import json
 import psycopg2
+import re
+
 
 DB_CONNECTION_STRING = os.getenv('DB_CONNECTION_STRING')
 conn = psycopg2.connect(DB_CONNECTION_STRING)
@@ -9,21 +11,34 @@ cursor = conn.cursor()
 
 
 def lambda_handler(event, context):
-    ideas = get_new_ideas_from_db()
+    try:
 
-    for idea in ideas:
-        idea_id = idea[0],
-        idea_text = idea[1]
+        ideas = get_new_ideas_from_db()
 
-        prompts = generate_prompts_from_idea(idea_text)
-        save_prompts_to_db(prompts, idea_id)
-        set_idea_as_done(idea_id)
+        for idea in ideas:
+            idea_id = idea[0],
+            idea_text = idea[1]
+
+            prompts = generate_prompts_from_idea(idea_text)
+            save_prompts_to_db(prompts, idea_id)
+            set_idea_as_done(idea_id)
+    except:
+        cursor.close()
+        conn.close()
+        return {
+            "statusCode": 500,
+            "body": json.dumps(
+                {
+                    "message": "Error generating prompts",
+                }
+            ),
+        }
 
     return {
         "statusCode": 200,
         "body": json.dumps(
             {
-                "message": "hello world this is yotam version 2",
+                "message": "Done generating prompts",
             }
         ),
     }
@@ -39,7 +54,10 @@ def get_new_ideas_from_db():
 
         return post_ideas
     except (Exception, psycopg2.Error) as error:
+        cursor.close()
+        conn.close()
         print(f"Error fetching ideas: {error}")
+
 
 def set_idea_as_done(idea_id):
     try:
@@ -49,11 +67,12 @@ def set_idea_as_done(idea_id):
 
         # Commit the transaction and close the cursor and connection
         conn.commit()
+
+    except (Exception, psycopg2.Error) as error:
         cursor.close()
         conn.close()
-        
-    except (Exception, psycopg2.Error) as error:
-        print(f"Error saving idea: {error}")    
+        print(f"Error saving idea: {error}")
+
 
 def generate_prompts_from_idea(idea):
     ai21.api_key = os.getenv("AI21_API_KEY")
@@ -74,14 +93,22 @@ def generate_prompts_from_idea(idea):
 
     answer = answer.completions[0].data.text
 
-    prompts = answer.split("\n")
-    prompts = filter(lambda x: x != "", prompts)
+    try:
+        # regex of Number followed by dot and space: "1. "
+        prompts = re.split(r'\d+\.\s', answer)
 
-    # removing the first 2 characters which "1. "
-    prompts = [prompt[3:] for prompt in prompts]
+        # Remove any empty strings resulting from the split
+        prompts = [prompt.strip() for prompt in prompts if prompt.strip()]
 
-    return prompts
-    
+        return prompts
+
+    except:
+        cursor.close()
+        conn.close()
+        raise f"""Could not parse answer from ai labs:
+            `{answer}`
+        """
+
 
 def save_prompts_to_db(prompts, idea_id):
     try:
@@ -97,10 +124,10 @@ def save_prompts_to_db(prompts, idea_id):
 
         # Commit the transaction and close the cursor and connection
         conn.commit()
-        cursor.close()
-        conn.close()
 
         print(f"generated prompts for idea {idea_id} saved")
 
     except (Exception, psycopg2.Error) as error:
+        cursor.close()
+        conn.close()
         print(f"Error saving idea: {error}")
