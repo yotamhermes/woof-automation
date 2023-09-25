ee8d339cd1360f7dc2291abc9317ad3b5e767b93
yotamhermes <yotamhermes@gmail.com>
2023-09-26 01:54:56 +0300

    tried env

--- webapp/src/Services/postSuggestions.js
+++ webapp/src/Services/postSuggestions.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_KEY = process.env.REACT_APP_WOOF_API_KEY;
-const URL = process.env.REACT_APP_WOOF_URL;
+const API_KEY = "aUIAPxb3pba5co7AXUp0V3S3C7ShrIjJ83W06VUh"; //process.env.REACT_APP_WOOF_API_KEY;
+const URL = "https://lomye7y9bc.execute-api.us-east-1.amazonaws.com/prod/"; // process.env.REACT_APP_WOOF_URL;
 
 const service = axios.create({
   baseURL: URL,
